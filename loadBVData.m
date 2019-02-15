c206bf1a63216b02f9bb89a355c1f90e6502614a
Olav Krigolson <krigolson@gmail.com>
2019-02-14 19:51:19 -0800

    Update
    Added script support

--- loadBVData.m
+++ loadBVData.m
@@ -1,4 +1,26 @@
-[filename, pathname, filterindex] = uigetfile('*.*','Pick a Brain Vision format .vhdr file to load');
+% function to load BRain Vision format data
+% by Olave Krigolson, Feb 2019
+% has EEGLAB dependecies and also dependencies on bva-io
+%
+% to have a popup select a file call EEG = loadBVData();
+% 
+% to script the call use EEG = loadBVData('fileName.vhdr');
 
-[EEG, com] = pop_loadbv(pathname, filename);
-EEG = pop_chanedit(EEG,'lookup','Standard-10-20-Cap81.ced')
+function EEG = loadBVData(varargin)
+
+    if size(varargin,1) == 0
+
+        [filename, pathname, filterindex] = uigetfile('*.*','Pick a Brain Vision format .vhdr file to load');
+        cd(pathname);
+        
+    else
+        
+        filename = varargin{1};
+        pathname = pwd;
+        
+    end
+
+    [EEG, com] = pop_loadbv(pathname, filename);
+    EEG = pop_chanedit(EEG,'lookup','Standard-10-20-Cap81.ced');
+    
+end
